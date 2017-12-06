6175a099c58d593b8257f88cc988cc413b1e9b3f
Mazen Morgan <morgan.mazen42@gmail.com>
2017-12-06 20:14:06 +0200

    Moving Ship around X and Y with Clamping !

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,14 +5,29 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [Tooltip("ms^-1")][SerializeField] float xSpeed = 1.0f;
+    [Tooltip("ms^-1")][SerializeField] float ySpeed = 1.0f;
+
+
+    void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
-	}
+	void Update ()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float xPos = Mathf.Clamp(rawNewXPos, -3.8f, 3.8f); //TODO parametarize
+        transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
+
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float yPos = Mathf.Clamp(rawNewYPos, -2.5f, 2.9f); //TODO parametarize
+        transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
+
+    }
 }
