fa7d9713598ae44aedc5aa4552c816134732e14b
Mazen Morgan <morgan.mazen42@gmail.com>
2017-12-08 20:07:48 +0200

    Tidying my project up

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] AudioClip splashClip;
+
+    AudioSource audioSource;
+    SceneLoader sceneLoader;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(splashClip);
+        StartCoroutine(sceneLoader.StartSplashMusic());
+	}
+	
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public IEnumerator StartSplashMusic()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(1);
+    }
+}
