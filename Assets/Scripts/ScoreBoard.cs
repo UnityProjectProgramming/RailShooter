ebbd07c2a165e2fa41737f2b945fec7ec8b455f8
Mazen Morgan <morgan.mazen42@gmail.com>
2018-01-03 23:49:52 +0200

    Adding ScoreBoard

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+    [SerializeField] int scorePerHit = 12;
+    int score;
+    Text scoreText;
+	void Start ()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    public void ScoreHit()
+    {
+        score += scorePerHit;
+        scoreText.text = score.ToString();
+    }
+
+}
